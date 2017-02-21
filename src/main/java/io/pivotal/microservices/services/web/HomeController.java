59bec56b89f9b63573228b4a5af83f09f759c79b
John1187 <john.solomon1187@gmail.com>
2017-02-21 18:05:19 +0530

    Master Branch Creation

---
+++ src/main/java/io/pivotal/microservices/services/web/HomeController.java
@@ -0,0 +1,24 @@
+package io.pivotal.microservices.services.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Home page controller.
+ * 
+ * @author Paul Chapman
+ */
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+	@RequestMapping("/user")
+	public String goUsers() {
+		return "user";
+	}
+
+}
